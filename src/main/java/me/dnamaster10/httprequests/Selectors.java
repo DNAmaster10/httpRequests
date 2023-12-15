62060e9f1bcfd88baa1e9cc795b28cfcc3ee35dc
Louis <44494235+DNAmaster10@users.noreply.github.com>
2023-12-15 20:29:58 +0000

    Improved readability, allow escaping of selectors + multi length selectors

--- src/main/java/me/dnamaster10/httprequests/Selectors.java
+++ src/main/java/me/dnamaster10/httprequests/Selectors.java
@@ -3,51 +3,71 @@ package me.dnamaster10.httprequests;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Selectors {
     //Contains methods for handling target selectors (@a, @p e.t.c)
     static Plugin p = HttpRequests.plugin;
+    //Used to select characters between "|" characters where the first one is not escaped with a backslash
+    private static final Pattern betweenSelector = Pattern.compile("(?<!\\\\\\\\)\\\\|([^\\\\|]+)\\\\|");
     private static String replaceSelectors(String value, String selector, String finalString) {
         //Takes in a selector and a string, replaces unescaped selectors with final string.
-        return Pattern.compile("(?<!\\\\)" + Pattern.quote(selector))
-                .matcher(value)
-                .replaceAll(finalString);
+        String pattern = "(?<!\\\\)\\|(" + selector + ")\\|";
+        return (value.replaceAll(pattern, finalString));
+    }
+    private static ArrayList<String> getOnlinePlayersD() {
+        //Returns an array list of online player display names
+        ArrayList<String> players = new ArrayList<>();
+        for (Player player : p.getServer().getOnlinePlayers()) {
+            players.add(player.getDisplayName());
+        }
+        return players;
+    }
+    private static ArrayList<String> getOnlinePlayersN() {
+        //Returns an array list of online player names
+        ArrayList<String> players = new ArrayList<>();
+        for (Player player : p.getServer().getOnlinePlayers()) {
+            players.add(player.getName());
+        }
+        return players;
+    }
+    private static ArrayList<String> getOnlinePlayersU() {
+        //Returns an array list of online player UUIDs
+        ArrayList<String> players = new ArrayList<>();
+        for (Player player : p.getServer().getOnlinePlayers()) {
+            players.add(String.valueOf(player.getUniqueId()));
+        }
+        return players;
     }
     public static String replaceSelectors(String value) {
-        //If selectors are present
-        if (value.contains("@a")) {
-            //Check for subselectors
-            if (value.contains("@ad")) {
-                //Replace player names
-                StringBuilder players = new StringBuilder();
-                for (Player player : p.getServer().getOnlinePlayers()) {
-                    players.append(player.getDisplayName()).append(",");
-                }
-                //Build string and remove last comma
-                String playersString = players.substring(0, players.length() - 1);
-                value = replaceSelectors(value, "@ad", playersString);
+        //Contains all matches, used to check whether a match has already been processed
+        ArrayList<String> matches = new ArrayList<>();
+
+        Matcher matcher = betweenSelector.matcher(value);
+        while (matcher.find()) {
+            //For every selector, handle and replace
+            String match = matcher.group(1);
+            if (matches.contains(match)) {
+                continue;
             }
-            if (value.contains("@an")) {
-                //Replace actual names
-                StringBuilder players = new StringBuilder();
-                for (Player player : p.getServer().getOnlinePlayers()) {
-                    players.append(player.getName()).append(",");
+            switch(match) {
+                case "@ad" -> {
+                    //Get list of player display names and replace in string
+                    value = replaceSelectors(value, "@ad", String.join(",", getOnlinePlayersD()));
                 }
-                //Build string and remove last comma
-                String playersString = players.substring(0, players.length() - 1);
-                value = replaceSelectors(value, "@an", playersString);
-            }
-            if (value.contains("@au")) {
-                //Replace UUIDs
-                StringBuilder players = new StringBuilder();
-                for (Player player : p.getServer().getOnlinePlayers()) {
-                    players.append(player.getUniqueId()).append(",");
+                case "@an" -> {
+                    //Get list of all actual player names and replace in string
+                    value = replaceSelectors(value, "@an", String.join(",", getOnlinePlayersN()));
                 }
-                //Build string and remove last comma
-                String playersString = players.substring(0, players.length() - 1);
-                value = replaceSelectors(value, "@au", playersString);
+                case "@au" -> {
+                    //Get a list of all player UUIDs and replace in string
+                    value = replaceSelectors(value, "@au", String.join(",", getOnlinePlayersU()));
+                }
             }
+            //Add this match to the matches array
+            matches.add(matcher.group(1));
         }
         return value;
     }
